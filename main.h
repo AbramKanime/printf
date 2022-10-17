3ac33835417c0c5f52536d172d1495e2c94c05f2
maranathaadesanya <adesanyamaranatha@yahoo.com>
2022-10-17 08:16:41 -0700

    Hot fix

--- main.h
+++ main.h
@@ -6,32 +6,33 @@
 
 /**
  * struct structprint - structure containing
- * @q: the location and method to translate data to characters.
- * @u: print function for specific type.
+ * @type: the location and method to translate data to characters.
+ * @f: print function for specific type.
  *
  * Return: int.
  */
 
-typedef struct structprint
+typedef struct fmt
 {
-	char *q;
-	int (*u)(char *format, va_list);
-} structype;
+	char *type;
+	int (*f)(char *format, va_list);
+} form_spec;
 
 int _putchar(char ch);
 int _puts(char *string);
 int printc(char *format, va_list);
 int printstr(char *format, va_list);
-int (*driver(char *format))(char *format, va_list);
-int _printf(char *format, ...);
-int printint(char *format, va_list pa);
+int _printf(const char *format, ...);
 int integer(int number);
-int contadordigit(int number);
 int _abs(int number);
 int printpercent(char *format, va_list pa);
 int printhex(char *format, va_list);
 int printHEX(char *format, va_list);
 int printocta(char *format, va_list);
-int print_unsign(char *format, va_list);
+int print_int(va_list numbers);
+int ch(va_list charact);
+int print_op(const char *format, format_spec *struct_arr, va_list list);
+int _strlen(char *string);
+int u_int(va_list number);
 
 #endif
